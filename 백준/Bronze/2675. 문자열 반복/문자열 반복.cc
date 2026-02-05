d375aac052cc3e3cad5d106e19a77f91c17d81f0
Minchae <mnc3320@icloud.com>
2026-02-05 22:25:05 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -8,19 +8,18 @@ using namespace std;
 int main() {
 	int t;
 	cin >> t;
-	for(int i = 0; i < t; i++){
+	
+	
+	while(t--){
 		int k = 0;
 		string s;
+		cin >> k >> s;
 		
-		cin >> k;
-		cin.ignore();
-		getline(cin, s);
-		
-		for(int j = 0; j < s.length(); j++)
-			for(int h = 0; h < k; h++)
-				cout << s[j];
+		for(char c : s)
+			for(int i = 0; i < k; i++)
+				cout << c;
 		cout << endl;
 	}
-	cout << endl;
+	
 	return 0;
 }
